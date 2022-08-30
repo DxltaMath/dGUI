cdb9ca14fd395a82cec791c0790e6f65d1111e35
gemsvido <gemsvido@gmail.com>
2022-08-30 10:57:12 -0700

    add declarations and a new hack

---
+++ declarations/delta.d.ts
@@ -0,0 +1,10 @@
+/** Accesed by `window.delta` in the js console. */
+declare export default class delta {
+
+    /** Always correct in timed problems */
+    public static doNotRandomize : boolean;
+
+    /** Allow escaping timed problems */
+    public static allowEscapingTimed : boolean;
+
+}
--- src/hacks/timed.ts
+++ src/hacks/timed.ts
@@ -4,8 +4,16 @@ import { delta } from "../utils/util";
 
 new Toggler(category.timed, "No Randomize", "Does not randomize math problems in timed challenges").setEnabled(async () => {
     delta.doNotRandomize = true;
-    return Toast.fire("Enabled!", "Timed problems will no longer be randomized!");
+    return Toast.fire("Enabled!", "Timed problems will no longer be randomized!", "success");
 }).setDisabled(async () => {
     delta.doNotRandomize = false;
-    return Toast.fire("Disabled!", "Timed problems will now be randomized!");
+    return Toast.fire("Disabled!", "Timed problems will now be randomized!", "success");
+});
+
+new Toggler(category.timed, "Allow Escaping", "Allows you to exit timed challenges even without pressing \"Stop\".").setEnabled(async () => {
+    delta.allowEscapingTimed = true;
+    return Toast.fire("Enabled!", "You can now exit running timed challenges!", "success");
+}).setDisabled(async () => {
+    delta.allowEscapingTimed = false;
+    return Toast.fire("Disabled!", "You now have to stop timed challenges before exiting them.", "success");
 });
